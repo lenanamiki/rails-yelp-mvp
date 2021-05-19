5c1ed19e46aa6169330f190449bf07a03c2bccff
Lena Namiki <lena.namiki@gmail.com>
2021-05-19 12:27:08 +0900

    Created restaurant and review models with associations and validations

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  CATEGORY = %w[chinese italian japanese french belgian].freeze
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+end
