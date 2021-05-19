c21ab2cfffbca22c4dfb8f6cf1fd2cb163dd6185
Lena Namiki <lena.namiki@gmail.com>
2021-05-19 15:48:16 +0900

    Added navbar to all pages linked to restaurants and new_restaurant path

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,11 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
@@ -19,5 +19,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
