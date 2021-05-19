c03556093608bed165e30c6d1c6184e702459baf
Lena Namiki <lena.namiki@gmail.com>
2021-05-19 12:46:25 +0900

    Created 5 fake restaurants in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Seeding database...'
+
+Restaurant.create!(
+    name: 'Panda Express',
+    address: '123 Shibuya street',
+    phone_number: '03-6583-8276',
+    category: 'chinese'
+  )
+
+Restaurant.create!(
+    name: 'Belgian Restaurant',
+    address: '141 Ebisu street',
+    phone_number: '03-6193-6188',
+    category: 'belgian'
+  )
+
+Restaurant.create!(
+    name: 'Gonpachi',
+    address: '888 Minami-azabu street',
+    phone_number: '03-1953-1319',
+    category: 'japanese'
+  )
+
+Restaurant.create!(
+    name: 'Bonjour',
+    address: '961 Meguro street',
+    phone_number: '03-8162-1618',
+    category: 'french'
+  )
+
+Restaurant.create!(
+    name: 'Esse Due',
+    address: '149 Akasaka street',
+    phone_number: '03-9695-5206',
+    category: 'italian'
+  )
+
+puts "DONE!"
